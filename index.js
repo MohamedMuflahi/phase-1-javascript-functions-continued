45fed6916335a2bb78308de7824a3eb0b1666536
mohamed muflahi <muflahimohamed88@gmail.com>
2022-01-31 16:37:47 -0500

    commit tree 18daf237c9be3253366746f79841421a10f4e7a6

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(x = 'roller-skate'){
+    return `This Saturday, I want to ${x}!`;
+}
+function mondayWork(x = 'go to the office'){
+    return `This Monday, I will ${x}.`;
+}
+function wrapAdjective(y){
+    
+    return function(x = 'special'){
+        return `You are ${y}${x}${y}!`
+    }
+}
